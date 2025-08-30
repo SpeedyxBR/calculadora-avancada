6e585ae6940c66755521fc7fd8e7b93e6f78550b
SpeedyxBR <andrel.cilva57@gmail.com>
2025-08-29 23:32:42 -0300

    feat(api): add POST handler for saving calculations

--- src/app/api/history/route.ts
+++ src/app/api/history/route.ts
@@ -1,4 +1,5 @@
-import { db } from "@/db/db";   
+// src/app/api/history/route.ts
+import { db } from "@/db/db";
 import { calculations } from "@/db/schema";
 import { NextResponse } from "next/server";
 
@@ -10,6 +11,33 @@ export async function GET() {
     console.error("Failed to fetch history:", error);
     return NextResponse.json(
       { message: "Error fetching history." },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const { expression, result } = body;
+
+    if (!expression || !result) {
+      return NextResponse.json({ message: "Invalid data" }, { status: 400 });
+    }
+
+    const newCalculation = await db
+      .insert(calculations)
+      .values({
+        expression,
+        result,
+      })
+      .returning();
+
+    return NextResponse.json({ success: true, calculation: newCalculation[0] });
+  } catch (error) {
+    console.error("Failed to save calculation:", error);
+    return NextResponse.json(
+      { message: "Error saving calculation." },
       { status: 500 }
     );
   }
